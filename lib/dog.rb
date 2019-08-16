e97ad7117b5744808ed56cd4d2f6fba210b6126e
James Britton <brittonjp@gmail.com>
2019-08-16 06:46:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,16 @@
 class Dog
+  
   def name=(dog_name)
     @this_dogs_name = dog_name
-  end
+  end 
   
-  def name 
+  def name
     @this_dogs_name
   end
+  
 end
 
 lassie = Dog.new
 lassie.name = "Lassie"
  
-puts lassie.name
+puts lassie.name #=> "Lassie"
